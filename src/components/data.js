83425f8bea1a4c7e5439bd595ee3897f5a3e0d53
tirupatikagne <testygo247@gmail.com>
2025-01-17 16:30:25 +0530

    new-video

--- src/components/data.js
+++ src/components/data.js
@@ -166,6 +166,7 @@ export const courses = [
       { id: 3,image:mern, title: "html part - 2", link: "https://drive.google.com/file/d/12XCTelDMqMFLELqaod8Ioz0B8iZ05KBS/view?usp=drive_link" },
       { id: 4,image:mern, title: "html part - 3", link: "https://drive.google.com/file/d/16yahk2vKPMMfk7elscbQ7bJ561TK1Ll6/view?usp=drive_link" },
       { id: 5,image:mern, title: "html part - 4", link: "https://drive.google.com/file/d/17N3FzlqMc_4rrj4HkBEWAXdiDa0n3pn8/view?usp=drive_link" },
+      { id: 6,image:mern, title: "html part - 5", link: "https://drive.google.com/file/d/19BXDVgEJOr6SEzFHG7AUfIXbmwpbb85R/view?usp=drive_link" },
 
     ],
   },
